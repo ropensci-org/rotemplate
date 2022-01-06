31a4f19db93d25ea4a66c9a13f1be1d6546447e6
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-01-06 13:21:41 +0100

    change

--- R/build-site.R
+++ R/build-site.R
@@ -24,8 +24,7 @@ build_ropensci_docs <- function(path = ".", destination = NULL, install = FALSE,
   override <- list(
     template = list(
       package = "rotemplate",
-      mathjax = need_mathjax(path),
-      params = NULL,
+      params = list(mathjax = need_mathjax(path)),
       path = NULL
     ),
     development = list(mode = 'release'),
