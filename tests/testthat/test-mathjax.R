c71dd8475542339943da1c7eb5f05405bcd315bc
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-03-28 11:02:33 +0200

    try fixing Windows failure (#89)

--- tests/testthat/test-mathjax.R
+++ tests/testthat/test-mathjax.R
@@ -12,15 +12,15 @@ test_that("adding mathjax works", {
   yaml <- yaml::read_yaml("_pkgdown.yml")
   yaml$mathjax <- TRUE
   yaml::write_yaml(yaml, "_pkgdown.yml")
-  expect_message({
-    expect_output({
+  expect_output({
+    suppressMessages(
       docs <- build_ropensci_docs(
         destination = "docs",
         install = FALSE,
         examples = FALSE)
-    })
+    )
   })
-  homepage <- xml2::read_html(file.path(docs, "index.html"))
+  homepage <- xml2::read_html(file.path(docs, "index.html"), encoding = "UTF-8")
   script <- xml2::xml_find_first(homepage, ".//head/script[@id='MathJax-script']")
   # If not present the class is xml_missing
   testthat::expect_s3_class(script, "xml_node")
@@ -37,15 +37,15 @@ test_that("NOT adding mathjax works", {
   usethis::local_project(proj)
   usethis::use_readme_md()
   usethis::use_pkgdown()
-  expect_message({
-    expect_output({
+  expect_output({
+    suppressMessages(
       docs <- build_ropensci_docs(
         destination = "docs",
         install = FALSE,
         examples = FALSE)
-    })
+    )
   })
-  homepage <- xml2::read_html(file.path(docs, "index.html"))
+  homepage <- xml2::read_html(file.path(docs, "index.html"), encoding = "UTF-8")
   script <- xml2::xml_find_first(homepage, ".//head/script[@id='MathJax-script']")
   testthat::expect_s3_class(script, "xml_missing")
 })
